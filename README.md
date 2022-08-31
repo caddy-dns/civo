e0961731d4f18166f2f206d732cc812027c7a5d2
Engin Diri <engin.diri@mail.schwarz>
2022-08-31 22:29:33 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+Scaleway module for Caddy
+===========================
+
+This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage DNS records with Civo.
+
+## Caddy module name
+
+```
+dns.providers.civo
+```
+
+## Config examples
+
+To use this module for the ACME DNS challenge, [configure the ACME issuer in your Caddy JSON](https://caddyserver.com/docs/json/apps/tls/automation/policies/issuer/acme/) like so:
+
+```json
+{
+	"module": "acme",
+	"challenges": {
+		"dns": {
+			"provider": {
+				"name": "civo",
+				"api_token": "{env.CIVO_TOKEN}"
+			}
+		}
+	}
+}
+```
+
+or with the Caddyfile:
+
+```
+# globally
+{
+	acme_dns civo {
+	    api_token      = "{env.CIVO_TOKEN}"
+	}
+}
+```
+
+```
+# one site
+tls {
+	dns civo {
+	    api_token      = "{env.CIVO_TOKEN}"
+	}
+}
+```
---
+++ civo.go
@@ -0,0 +1,69 @@
+package civo
+
+import (
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/libdns/civo"
+)
+
+// Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
+type Provider struct{ *civo.Provider }
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.civo",
+		New: func() caddy.Module { return &Provider{new(civo.Provider)} },
+	}
+}
+
+// Provision sets up the module. Implements caddy.Provisioner.
+func (p *Provider) Provision(ctx caddy.Context) error {
+	p.Provider.APIToken = caddy.NewReplacer().ReplaceAll(p.Provider.APIToken, "")
+	return nil
+}
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+//	civo {
+//	    api_token string
+//	}
+//
+// **THIS IS JUST AN EXAMPLE AND NEEDS TO BE CUSTOMIZED.**
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "api_token":
+				if p.Provider.APIToken != "" {
+					return d.Err("API token already set")
+				}
+				if d.NextArg() {
+					p.Provider.APIToken = d.Val()
+				}
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+	if p.Provider.APIToken == "" {
+		return d.Err("missing API token")
+	}
+	return nil
+}
+
+// Interface guards
+var (
+	_ caddyfile.Unmarshaler = (*Provider)(nil)
+	_ caddy.Provisioner     = (*Provider)(nil)
+)
